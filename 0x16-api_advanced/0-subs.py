4a76acefbcb74c04599d793bfcb216c62b2074bc
leon chepkwony <leonchepkwony2@gmail.com>
2024-01-26 12:30:37 +0300

    changed user token

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,14 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
+    else:
+        print(f"Error: {response.status_code}")
     results = response.json().get("data")
     return results.get("subscribers")
